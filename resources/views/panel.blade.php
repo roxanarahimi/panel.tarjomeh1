57a1b9a13032e437c7d96afe95ad1b78a19091ec
Roxana <ms.roxanarahimi@gmail.com>
2022-10-28 11:29:28 +0330

    p

--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -39,7 +39,6 @@
 {{--<script src="{{ asset('js/bootstrap.bundle.min.js', true) }}"></script>--}}
 <script src="{{ asset('js/app.js') }}"></script>
 {{--<script src="{{ asset('bootstrap-5.2.0-beta1-dist/js/bootstrap.bundle.js') }}"></script>--}}
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 <script src="{{ asset('js/themes.js') }}"></script>
