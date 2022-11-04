acac4d55c0310a72b6634b1caae85a61b3608d34
Roxana <ms.roxanarahimi@gmail.com>
2022-11-04 19:16:46 +0330

    px

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -38,7 +38,7 @@ class FileController extends Controller
             $pngData = $image->getImagesBlob();
             return response($image, $pngData);
         }catch (\Exception $exception){
-            return response($exception,$exception->getCode());
+            return response($exception,(integer)$exception->getCode());
         }
 
 //        echo str_starts_with($pngData, "\x89PNG\r\n\x1a\n") ? 'Ok' : 'Failed';
