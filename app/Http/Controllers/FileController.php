34143882903b583a975680ddc411b68fafc7887e
Roxana <ms.roxanarahimi@gmail.com>
2022-11-05 19:48:17 +0330

    px

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -39,7 +39,7 @@ class FileController extends Controller
             $image->setImageFormat("jpg");
 //            return $image;
             $txt = (new TesseractOCR($image))
-//                ->executable('C:\Program Files\Tesseract-OCR\tesseract.exe')
+                ->executable('Tesseract-OCR\tesseract.exe')
                 ->run();
             $txt = str_replace("\n", ' ', $txt);
             $count = explode(' ', $txt);
